8bfd056c2da5c089149b32037dd8f224b9f26929
Alexander Gazizov <alexandr.bbm@gmail.com>
2017-03-12 17:30:38 +0700

    Fixed ip in header-parser microservice

--- header-parser/index.js
+++ header-parser/index.js
@@ -3,12 +3,23 @@ const router = require('express').Router();
 
 router.get(`/`, (req, res) => {
 	const result = {
-		"ipaddress": req.ip,
+		"ipaddress": getIp(req),
 		"language": req.headers['accept-language'].split(',')[0],
 		"software": findFirstInsideBrackets(req.headers['user-agent'])
 	};
 	res.send(result);
 });
+
+const getIp = (req) => {
+	let ipAddr = req.headers["x-forwarded-for"];
+	if (ipAddr){
+		const list = ipAddr.split(",");
+		ipAddr = list[list.length-1];
+	} else {
+		ipAddr = req.connection.remoteAddress;
+	}
+	return ipAddr;
+};
 
 const findFirstInsideBrackets = (str) => {
 	const result = str.match(/\((.*?)\)/)[1];
